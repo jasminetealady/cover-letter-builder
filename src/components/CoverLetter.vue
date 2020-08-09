8cfc0cef566b98697f6d3b8a3fc3caf17c87cb84
Razelle McCarrick <razelle@satyaorthopedic.com>
2020-08-08 21:53:42 -0400

    formatting

--- src/components/CoverLetter.vue
+++ src/components/CoverLetter.vue
@@ -15,17 +15,14 @@
           isCurrentlyEmployed ? 'current position' : 'most recent experience'
         }}
         as an
-        <EditorInput v-model="store.coverLetter.previousOrCurrentPosition" />
-        at
+        <EditorInput v-model="store.coverLetter.previousOrCurrentPosition" /> at
         <EditorInput v-model="store.coverLetter.previousOrCurrentCompany" />, I
         <EditorInput v-model="store.coverLetter.firstPositionAction" />
         {{ ' ' }}
-        <EditorInput v-model="store.coverLetter.firstPositionNoun" />
-        with
+        <EditorInput v-model="store.coverLetter.firstPositionNoun" /> with
         <EditorInput v-model="store.coverLetter.firstPositionTools" />. In my
         previous experience as a
-        <EditorInput v-model="store.coverLetter.secondPreviousPosition" />
-        at
+        <EditorInput v-model="store.coverLetter.secondPreviousPosition" /> at
         <EditorInput v-model="store.coverLetter.secondPreviousCompany" />, I
         <EditorInput v-model="store.coverLetter.secondPositionAction" /> the
         <EditorInput v-model="store.coverLetter.secondPositionNoun" /> with
